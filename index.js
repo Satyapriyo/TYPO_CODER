990cfd8b2d6620bdfd72a52a009c43c672d1f5fd
Satyapriyo Biswas <satyapriyoak47@gmail.com>
2023-01-30 22:02:40 +0530

    the first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+let ans = document.getElementById("final_ans");
+let txt = document.getElementById("txt");
+let type = document.querySelector("input")
+if(type.value !== null){
+    console.log(type.value)
+}
